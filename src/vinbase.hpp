ee76877935a2f85a61146ba68833f4510af82e0c
HITOA <charli.poirier@gmail.com>
2022-08-21 11:35:13 +0200

    Add base type

--- src/vinbase.hpp
+++ src/vinbase.hpp
@@ -18,6 +18,22 @@
 #error "NO PLATFORM DEFINED"
 #endif
 
+#ifndef __GNUC__
+#if defined(_WIN32) || defined(_WIN64)
+#ifdef _WIN64
+#define VIN_64
+#else
+#define VIN_32
+#endif
+#endif
+#else
+#if defined(__x86_64__) || defined(__ppc64__)
+#define VIN_64
+#else
+#define VIN_32
+#endif
+#endif
+
 #ifdef _DEBUG
 #define VIN_DEBUG
 #endif
@@ -27,3 +43,21 @@
 #define VIN_ENABLE_HARD_ASSERT
 #define VIN_ENABLE_SOFT_ASSERT
 #endif // DEBUG
+
+namespace Vin {
+	typedef unsigned char byte;
+
+	typedef long long i64;
+	typedef unsigned long ui64;
+
+	typedef int i32;
+	typedef unsigned int ui32;
+
+#ifdef VIN_64
+	typedef i64 isize;
+	typedef ui64 usize;
+#else
+	typedef i32 isize;
+	typedef ui32 usize;
+#endif
+}
